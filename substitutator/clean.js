476e15585ee478b31306d8cb7586e1580bf95f46
gabriel <noreply@example.com>
2018-11-24 16:25:37 +1300

    idempotent clean script

--- substitutator/clean.js
+++ substitutator/clean.js
@@ -8,7 +8,8 @@
 //  License: GPL v3
 //
 
-const { readdir, lstat, unlink, rmdir } = require('fs')
+const { readdir, unlink, rmdir } = require('fs')
+const { lstat, access, constants } = require('fs')
 const { EOL } = require('os')
 
 const RED_COLOR = '\x1b[31m'
@@ -17,10 +18,26 @@ const DEFAULT_COLOR = '\x1b[0m'
 
 // Delete the ./dist directory
 
-deleteDirectory('./dist')
+deleteIfExists('./dist')
 
 
 ///
+
+
+function deleteIfExists(directory) {
+  return p_access_ok(directory)
+    .then(getDirectory)
+    .then(deleteDirectory)
+    .catch(resume)
+
+  function getDirectory() {
+    return directory
+  }
+
+  function resume() {
+    return
+  }
+}
 
 
 function deleteDirectory(path) {
@@ -52,14 +69,16 @@ function deleteFileOrDirectory(fullPath) {
   function detectAndDelete(stats) {
     if (stats.isDirectory())  return deleteDirectory(fullPath)
     if (stats.isFile())  return p_unlink(fullPath)
-    return Promise.reject('Child is neither a file not a directory')
+    return Promise.reject(new Error('Child is neither a file not a directory'))
   }
 }
 
 
 function stop(err) {
-  if (!err || !err.message) throw err
-  console.log(RED_COLOR, err.message, DEFAULT_COLOR, EOL)
+  if (err && err.message)  console.log(RED_COLOR, err.message, DEFAULT_COLOR, EOL)
+  if (err && !err.message)  console.log(RED_COLOR, 'Failure with no error message', EOL, err, DEFAULT_COLOR, EOL)
+  if (!err)  console.log(RED_COLOR, 'Failure with unspecified error', DEFAULT_COLOR, EOL)
+  process.exit(1)
 }
 
 
@@ -78,18 +97,6 @@ function p_readdir(path) {
     readdir(path, function callback(err, files) {
       if (err)  reject(err)
       resolve(files)
-    })
-  }
-}
-
-
-function p_lstat(path) {
-  return new Promise(executor)
-
-  function executor(resolve, reject) {
-    lstat(path, function callback(err, stats) {
-      if (err)  reject(err)
-      resolve(stats)
     })
   }
 }
@@ -114,6 +121,30 @@ function p_rmdir(path) {
   function executor(resolve, reject) {
     trace('rmdir ', path)
     rmdir(path, function callback(err) {
+      if (err)  reject(err)
+      resolve()
+    })
+  }
+}
+
+
+function p_lstat(path) {
+  return new Promise(executor)
+
+  function executor(resolve, reject) {
+    lstat(path, function callback(err, stats) {
+      if (err)  reject(err)
+      resolve(stats)
+    })
+  }
+}
+
+
+function p_access_ok(path) {
+  return new Promise(executor)
+
+  function executor(resolve, reject) {
+    access(path, constants.F_OK, function callback(err) {
       if (err)  reject(err)
       resolve()
     })
